91882a7c5a920a37e391175055ea2516d1ec3c63
ceepu8 <caophuonguyen132002@gmail.com>
2023-08-03 00:38:18 +0700

    feat(navbar): create homepage navbar

---
+++ components/ui/button.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/libs/utils";
+import React, { forwardRef } from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          "w-auto rounded-full bg-black border-transparent px-5 py-3 text-white font-semibold hover:opacity-75",
+          "disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
