13ea19b3a8b04fc229c96bfe4569b6dc06c71f2b
ceepu8 <caophuonguyen132002@gmail.com>
2023-08-03 01:50:53 +0700

    feat(billboard): add carousel on homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,27 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("64c8b931634547b44a95ee47");
+
+  console.log(products);
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-7 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
