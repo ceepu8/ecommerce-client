21cbc95f1a1fbe56c0033dd02a50b434d2199dbf
ceepu8 <caophuonguyen132002@gmail.com>
2023-08-06 15:06:27 +0700

    feat(modal): build onPreview to open modal

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,10 +2,12 @@
 
 import Currency from "@/components/ui/currency";
 import IconButton from "@/components/ui/icon-button";
+import usePreviewModal from "@/hooks/user-preview-modal";
 import { Product } from "@/types";
 import { Expand, ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
 
 interface ProductCardProps {
   data: Product;
@@ -13,9 +15,16 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   const router = useRouter();
+  const previewModal = usePreviewModal();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
   };
 
   return (
@@ -34,7 +43,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
       <div className="opacity-0 group-hover:opacity-100 transition w-full px-6 bottom-5">
         <div className="flex gap-x-6 justify-center">
           <IconButton
-            onClick={() => {}}
+            onClick={onPreview}
             icon={<Expand size={20} className="text-gray-600" />}
           />{" "}
           <IconButton
